4eae414178db920cbc3df110b1ef8c1c6d18145f
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-02 17:18:59 +0100

    modifiche struttura più generazione password

--- script.js
+++ script.js
@@ -16,33 +16,36 @@ ANALISI
 4 - Chiedere all'utente il suo cognome 
 5 - Salvare la risposta nella variabile
 6 - Chiedere all'utente il suo colore preferito
-7-  Salvare la risposta nella variabile
-8 - Inserisco nella pagina la password generata
+7 - Salvare la risposta nella variabile
+8 - Compongo la password 
+9 - Inserisco nella pagina la password generata
 */
 
 // SVOLGIMENTO
-// 1 - Prendo gli elementi dalla pagina
-const nameSubject = document.getElementById("name");
-// console.log(nameUser);
-
-const surnameSubject = document.getElementById("surname");
-// console.log(surnameUser);
-
-const colorFavorite = document.getElementById("color");
-// console.log(colorUser);
-
-const myNumber = document.getElementById("number");
-// console.log(myNumber);
+// 1 - Prendo l'elemento dalla pagina
+const passwordSubject = document.getElementById("name");
+console.log(passwordSubject);
 
 
 // 2e3 - Chiedo all'utente come si chiama e lo salvo in variabile
 const firstName = prompt("Qual è il tuo nome?" , "alessandro");
-// console.log(firstName);
+console.log(firstName);
+
 
 // 4e5 - Chiedo all'utente il suo cognome e lo salvo nella variabile
 const lastName = prompt("Qual è il tuo cognome?" , "lembo");
-// console.log(lastName);
+console.log(lastName);
+
 
 // 6e7 - Chiedo all'utente il suo colore preferito e lo salvo in variabile
 const colorChoosen = prompt("Qual è il tuo colore preferito?" , "verde");
 console.log(colorChoosen);
+
+
+// 8 - Compongo la password
+const password = firstName + lastName + colorChoosen + "21";
+console.log(password);
+
+
+// 9 - Inserico la password generata nella pagina
+passwordSubject.innerText = password;
